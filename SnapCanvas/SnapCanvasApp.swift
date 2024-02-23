5b94d33ccad16cf7303d35a9dde360f2c420792a
Mohanna <mohannazakizadeh@gmail.com>
2024-02-23 17:57:09 +0100

    Initial Commit

---
+++ SnapCanvas/SnapCanvasApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnapCanvasApp.swift
+//  SnapCanvas
+//
+//  Created by Mohanna Zakizadeh on 2/23/24.
+//
+
+import SwiftUI
+
+@main
+struct SnapCanvasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
